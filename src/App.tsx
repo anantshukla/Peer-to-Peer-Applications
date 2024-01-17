df5e7566997aab6da5520b24d4614b395ae93b8c
Anant Shukla <accounts@anantshukla.com>
2024-01-16 19:34:00 -0500

    Added firebase config along with basic html page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from 'firebase/app';
+import { error } from 'console';
+
 function App() {
+
+  const app = initializeApp(firebaseConfig);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='videoContainer'>
+          <span>
+            <h3>Local Stream</h3>
+            <video id="localVideo" autoPlay playsInline></video>
+          </span>
+          <span>
+            <h3>Remote Stream</h3>
+            <video id="remoteVideo" autoPlay playsInline></video>
+          </span>
+        </div>
+
+        <button onClick={getPermissions}>Get Permissions</button>
+        <h2>2. Create a new Call</h2>
+        <button onClick={createCall} disabled>Create Call Link</button>
+
+
       </header>
     </div>
   );
 }
+
+function getPermissions() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+    .then(mediaContent => {
+      const localVideo: HTMLVideoElement = document.getElementById('localVideo') as HTMLVideoElement;
+      localVideo.srcObject = mediaContent;
+      const callButton: HTMLButtonElement = document.getElementById('callButton') as HTMLButtonElement;
+      callButton.disabled = false;
+    })
+    .catch(error => {
+      // prompt user that permission was denied
+      window.alert('Permission denied');
+    });
+}
+
+function createCall() {
+  // create a new call
+
+}
+
 
 export default App;
